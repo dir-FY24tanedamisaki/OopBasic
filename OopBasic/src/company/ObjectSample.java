38c1120391487f076232e4da0e9059c56c51abad
misaki.taneda <misaki.taneda.j847@dirbato.co.jp>
2024-04-08 15:00:09 +0900

    Lesson3コミット

---
+++ OopBasic/src/company/ObjectSample.java
@@ -0,0 +1,30 @@
+package company;
+
+import java.lang.invoke.VarHandle;
+
+public class ObjectSample {
+
+    public static void main(String[] args) {
+        //インスタンスの作成
+        var department = new Department("営業部", "xx", 1000000);
+        var employee = new Employee("鈴木",department,"課長",100);
+        
+        //インスタンスの呼び出し
+        employee.report();
+        employee.report(2);
+        employee.joinMeeting();
+        
+        //一行空ける
+        System.out.println("");
+        
+        //インスタンスの作成
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Employee("田中", devDepartment, "一般社員", 88);
+        
+        //メソッドの呼び出し
+        engineer.report();
+        engineer.joinMeeting();
+
+    }
+
+}
